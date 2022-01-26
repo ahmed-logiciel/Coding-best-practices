722c23f5fefef7e593718c838b945b71fdf495bc
Muhammad Ahmed <ahmed@logicielservice.com>
2022-01-26 15:06:43 +0500

    file logic in a separate class

---
+++ helper/fileReader.py
@@ -0,0 +1,42 @@
+from models.object import Object
+
+class FileReader:
+
+    def __init__(self, path: str) -> None:
+        print("File reader init")
+        self.m_path = path
+        self.m_file = 0
+
+    def __open(self) -> bool:   # private method, abstracted away from user
+        try:
+            self.m_file = open(self.m_path, 'r')
+            return True
+        except:
+            print("Exception thrown. File path specified is incorrect")
+            return False
+    
+    def setPath(self, path: str) -> bool:   # setter
+        if path == "":
+            return False
+        self.m_path = path
+        return True
+    
+    def getContent(self, objList: list) -> bool:    # reads file and stores in the referenced list
+        objList.clear()    # empty it first
+        if (self.__open()):
+            strList = self.m_file.readlines()
+            for i in strList:
+                obj = i.strip().split()
+                if len(obj) != 3:                   # this three is a magic number, think of logic to fix
+                    print("Wrong number of fields read")
+                    return False
+                else:
+                    objList.append(Object(obj[0], obj[1], obj[2]))
+            return True
+    
+    def __del__(self):  # automatically called
+        print("File reader destructor")
+        if (type(self.m_file) != int):
+            self.m_file.close()
+
+
---
+++ main.py
@@ -0,0 +1,28 @@
+from models.object import Object
+from helper.fileReader import FileReader
+
+def compare(objA, objB):
+    totalCompares = min(len(objA), len(objB))
+
+    for i in range(totalCompares):
+        if objA[i] == objB[i]:
+            print(i, "matched", objA[i], objB[i])
+        else:
+            print(i, "unmatched", objA[i], objB[i])
+
+def impl():
+
+    readerA = FileReader(r"data\objectsA.txt")
+    readerB = FileReader(r"data\objectsB.txt")
+
+    objA = []
+    objB = []
+
+    if (readerA.getContent(objA) and readerB.getContent(objB)):
+        compare(objA, objB)
+    else:
+        print("unable to fetch valid content from file")
+
+
+if __name__ == "__main__":
+    impl()
